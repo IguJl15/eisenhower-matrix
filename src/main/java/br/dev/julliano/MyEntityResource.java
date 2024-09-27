50092a4a64bdfaa7c15ce8498172d1e6ab79a155
quarkusio <no-reply@quarkus.io>
2024-09-27 20:42:46 +0000

    Initial commit

---
+++ src/main/java/br/dev/julliano/MyEntityResource.java
@@ -0,0 +1,6 @@
+package br.dev.julliano;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+}
